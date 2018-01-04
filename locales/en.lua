6880280c7f37f3c7b7c5b704bc261966500ab4a6
fiber-hosting <info.itegem@gmail.com>
2018-01-04 16:20:48 +0100

    en.lua
    Locales['en'] = {
      -- Cells
      ['open_all'] = 'Open all the cells',
      ['close_all'] = 'Close all the cells',
      ['floor1'] = 'RDC',
      ['floor2'] = '1ER',
      ['cell'] = 'cell',
      ['cells'] = 'cells',
    
      -- Hints
      ['cells_menu'] = 'press on ~INPUT_CONTEXT~ to manage the cell doors',
      ['take_shower'] = 'press on ~INPUT_CONTEXT~ to take a shower',
    
      -- MISC
      ['blip_jail'] = 'prison',
    
    
    }

---
+++ locales/en.lua
@@ -0,0 +1,18 @@
+Locales['en'] = {
+  -- Cells
+  ['open_all'] = 'Open all the cells',
+  ['close_all'] = 'Close all the cells',
+  ['floor1'] = 'RDC',
+  ['floor2'] = '1ER',
+  ['cell'] = 'cell',
+  ['cells'] = 'cells',
+
+  -- Hints
+  ['cells_menu'] = 'press on ~INPUT_CONTEXT~ to manage the cell doors',
+  ['take_shower'] = 'press on ~INPUT_CONTEXT~ to take a shower',
+
+  -- MISC
+  ['blip_jail'] = 'prison',
+
+
+}
